638318471c3e2151eebb95193f8f51a4d735ec6c
mgading13 <145760642+mgading13@users.noreply.github.com>
2023-10-31 23:55:08 +0700

    Add files via upload

---
+++ percobaan6/MainActivity.java
@@ -0,0 +1,33 @@
+package com.gading.percobaan6;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+    private TabLayout tabLayout;
+    private AppBarLayout appBarLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tabLayout = (TabLayout) findViewById(R.id.tab_layout_id);
+        appBarLayout = (AppBarLayout) findViewById(R.id.appBar_id);
+        viewPager = (ViewPager) findViewById(R.id.viewpager_id);
+        ViewPagerAdapter adapter = new ViewPagerAdapter((getSupportFragmentManager()));
+
+        adapter.AddFragment(new FragmentHome(), "Home");
+        adapter.AddFragment(new FragmenExplore(), "Explore");
+        adapter.AddFragment(new FragmenAccount(), "Account");
+
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+}
+
